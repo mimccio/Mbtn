50f61ed6311e39b2e03493c6c3d09eb2934cbe18
Michael Maccio <miklmccio@gmail.com>
2017-04-17 18:20:13 +0200

    try 6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import palette from './config/colorPalette'
 
 // import font family
-import { font } from '../m-btn/settings/styleSettings'
+import { font } from './settings/styleSettings'
 
 // import styles settings
 import {
